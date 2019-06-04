498fb76e4edf2705e1c4f2f5f85729d80f8ea5eb
lidertic <lidertic@gmail.com>
2019-06-04 12:24:11 +0200

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  open = false;
+  position = 'left';
+  showCloseButton = true;
+  showBackdrop = true;
+  hideOnBackdropClick = true;
+
+  togglePosition() {
+    if (this.position === 'right') {
+      this.position = 'bottom';
+    } else if (this.position === 'bottom') {
+      this.position = 'left';
+    } else if (this.position === 'left') {
+      this.position = 'top';
+    } else if (this.position === 'top') {
+      this.position = 'right';
+    }
+  }
+}
